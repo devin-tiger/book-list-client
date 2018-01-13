baddda7766519abdd8fc5b04f9010e5ff91fa15f
devintyler26 <devincunningham@allegropediatrics.com>
2018-01-13 09:58:29 -0800

    testing delete button

--- scripts/views/book-detail-page.js
+++ scripts/views/book-detail-page.js
@@ -3,14 +3,23 @@ var app = app || {};
 (module => {
     const bookDetailPage = {}
 
-    const $view = $('#book-detail-page')
+    const $page = $('#book-detail-page')
 
     bookDetailPage.init = (book) => {
+        $('#book-details').empty()
         $('.page').hide()
-        $('#book-details').append(`<li data-id="${book.id}">${book.title}:${book.author}:<img src="${book.image_url}"></li>`)
+        $('#book-details').append(`<li data-id="${book.id}">${book.title}:${book.author}:<img src="${book.image_url}">
+        <input type="button" id="delete-button" value="Delete"/></li>`)
+        $page.show()
 
 
     }
-    $view.show()
+
+    $('#delete-button').one('click', 'input', (e) => {
+        const deleteId = $(e.target).data('id')
+        console.log('trying to delete book with id:', deleteId)
+        app.Book.delete(deleteId)
+    })
+
         module.bookDetailPage = bookDetailPage
 })(app)
