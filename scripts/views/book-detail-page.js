bb95310bcfc8fb468c544808d98fe5fd7e190399
devintyler26 <devincunningham@allegropediatrics.com>
2018-01-13 17:38:14 -0800

    password working

--- scripts/views/book-detail-page.js
+++ scripts/views/book-detail-page.js
@@ -6,6 +6,9 @@ var app = app || {};
     const $page = $('#book-detail-page')
 
     bookDetailPage.init = (book) => {
+        let realToken = '1234'
+        let getToken = localStorage.getItem('Token');
+        let token = JSON.parse(getToken)
         $('#book-details').empty()
         $('.page').hide()
         $('#book-details').append(`<li data-id="${book.id}">
@@ -14,18 +17,22 @@ var app = app || {};
         <img src="${book.image_url}"><br>${book.description}<br>
         <input type="button" class="delete admin" value="Delete"/><br>
         <input type="button" class="update admin" value="Update"/></li>`)
+        if (token === realToken) {
+            $('.admin').show()
+            $('.logged-in').hide() 
+        }else{$('.admin').hide()}
         $page.show()
-
+        
         $('#book-details').one('click', '.delete', (e) => {
             const id = $(e.target).parent().data('id')
             const confirmed = confirm('Are you sure you want to delete this book?')
             console.log('trying to delete book with id:', id)
-
+            
             if (confirmed){
-            app.Book.deleteOne(id)
+                app.Book.deleteOne(id)
             }
         })
-
+        
         
         $('#book-details').one('click', '.update', (e) => {
             const id = $(e.target).parent().data('id')
@@ -33,6 +40,7 @@ var app = app || {};
             page('/books/update/' + id)
             
         })
+
 
     }
 
